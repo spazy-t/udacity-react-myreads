e253d71202f8b4c2a16700f8cf645dfc6b870fbe
Tom Schrimshaw <tschrimshaw@gmail.com>
2020-07-15 14:45:02 +0100

    feat: waits for user typing
    waits to search for books until user stops typing.
    Also shows a warning on screen if no books found.

--- src/components/SearchScreen.js
+++ src/components/SearchScreen.js
@@ -9,17 +9,21 @@ import * as booksApi from '../BooksApi'
 import BookContainer from './BookContainer'
 
 class SearchScreen extends Component {
-    //value is for the search input text
-    state = {
-        value: '',
-        searchedBooks: []
+    constructor(props) {
+        super(props)
+        //value is for the search input text
+        this.state = {
+            value: '',
+            searchedBooks: []
+        }
+
+        this.delayTimer = ''
     }
 
     //called when text entered into input, uses value to search books api then
     //sets state of current value and searched books to pass to books container
     handleChange = (evt) => {
         const targetValue = evt.target.value
-        console.log('handleChange', targetValue)
 
         //if search value is empty reset the state to blank and avoid POST error
         if(targetValue === '') {
@@ -27,21 +31,41 @@ class SearchScreen extends Component {
                 value: '',
                 searchedBooks: []
             })
+            document.getElementById('no-books-found').setAttribute('style', 'display: none')
             return
-        }
-        
-        booksApi.search(targetValue)
-        .then((books) => {
+        } else {
             this.setState({
-                value: targetValue,
-                searchedBooks:  books !== undefined && books.length > 0 ? [...books] : []
+                value: targetValue
             })
-        })
-        .catch(e => {
-            console.log('error', e)
-        })
+        }
+        //use timer so it doesn't search on every single letter, waits for user to stop typing
+        clearTimeout(this.delayTimer)
+        this.delayTimer = setTimeout(() => {
+            booksApi.search(this.state.value)
+            .then((books) => {
+                this.setState({
+                    searchedBooks:  books !== undefined && books.length > 0 ? [...books] : []
+                })
+            })
+            .then(() => {
+                this.searchSuccessful()
+            })
+            .catch(e => {
+                console.log('error', e)
+            })
+        }, 1000)
     }
-    //TODO: a way of timeout the input so it's not one character at a time for search i.e. slow!
+
+    searchSuccessful = () => {
+        const noBooksMsg = document.getElementById('no-books-found')
+
+        if(this.state.value.length !== 0 && this.state.searchedBooks.length === 0) {
+            noBooksMsg.setAttribute('style', 'display: block')
+        } else {
+            noBooksMsg.setAttribute('style', 'display: none')
+        }
+    }
+
     render() {
         return(
             <div className='search-screen'>
@@ -63,6 +87,7 @@ class SearchScreen extends Component {
                         shelfBooks={this.props.shelvedBooks}
                     />
                 )}
+                <div id='no-books-found'>No books found!</div>
             </div>
         )
     }
